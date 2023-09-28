5f3fdba96fc321b5f68148dfd99e8ba7033a0ad8
4ghora <godguymicky@gmail.com>
2023-09-28 13:06:39 +0530

    ✅ Advanced Version Added

--- Network Scanner/Advanced Network Scanner.py
+++ Network Scanner/Advanced Network Scanner.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 from scapy.all import ARP, Ether, srp, ls
-
+from argparse import ArgumentParser
 def scan(ip):
     ar = ARP(pdst=ip) # ar means arp request, creating a arp request 
     broadcast = Ether(dst="ff:ff:ff:ff:ff:ff") # seting destination mac to broadcast mac
     arb = broadcast/ar # Combining broadcast and arp request together
-    ans = srp(arb, timeout=1, verbose=False)[0] # srp is used to send packets, ans = location where answered packets are stored, unans = location where unanswered packets are stored.
+    ans = srp(arb, timeout=5, verbose=False)[0] # srp is used to send packets, ans = location where answered packets are stored, unans = location where unanswered packets are stored.
+    
     #ls(ans)
-    print("\nIP\t\tMac Address\n-------------------------------------")
 
     el = [] # Empty List Used By dict (Dictionary Inside Loop) Appending Information.
 
@@ -18,8 +18,22 @@ def scan(ip):
         el.append(dict) # Appending dict(dictionary) to el(empty list).
 
 # [1] will help us in fethching only the information we need from the list, [1] is index.
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
-    print(el)
+        #print(element[1].psrc + "\t\t" + element[1].hwsrc)
+    return el
 
-get_ip = input("Enter An IP Like 192.168.1.1 or 192.168.1.1/24: ")
-scan(get_ip)
+def pr(resl): #function to print the result, pr means print result and resl means results list.
+    print("\nIP\t\t\tMac Address\n-------------------------------------")
+    for clients in resl:
+        print(clients["ip"] + "\t\t" + clients["mac"])
+
+def get_args(): # used to get arguments directly from terminal
+    parser = ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP/IP Range")
+    return parser.parse_args()
+    
+ 
+#get_ip = input("Enter An IP Like 192.168.1.1 or 192.168.1.1/24: ")
+# Comment Whole get_args() function and 38th Line Before Uncommenting Below Code.
+# pr(scan(input("Enter An IP Like 192.168.1.1 or 192.168.1.1/24: "))) #
+pr(scan(get_args().target)) #
+#print(scan_results)
