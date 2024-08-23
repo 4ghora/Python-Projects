f873ef9bd670d04dc6e6bb3c52836976b03fd24e
4ghora <godguymicky@gmail.com>
2024-08-23 12:40:20 +0530

    ✅ Added Sample Table Extractor (improved)

--- Fun/Simple Table Extraction.py
+++ Fun/Simple Table Extraction.py
@@ -1,3 +1,9 @@
-import pandas as pd
+# Pandas is a software library written for the Python programming language for data manipulation and analysis.
+import pandas as pd # We imported pandas as pd and extracted tables from the provided URL
 simpsons = pd.read_html('https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes_(seasons_1%E2%80%9320)#Season_1_(1989%E2%80%9390)')
-print (len(simpsons[10]))
+index = int(input("Enter The Table Number (MAX: 23) : "))
+print ("Number of tables present in webpage " + str(len(simpsons))) # Counted how many are table are present in the web page with first print statement
+print ("Number of columns preset in table " + str(len(simpsons[index]))) # Counted how many columns are present in at a particular index of table.
+print (str(simpsons[index])) # Extracting All Table contents
+
+# No error handling at this point, Maximum acceptable index number is 23
